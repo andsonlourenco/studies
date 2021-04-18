91f8f2cea901a8e2af96a42fed01b7c3b0f3c87e
Andson Lourenco <andson-lourenco@hotmail.com>
2021-04-17 21:40:46 -0300

    Variable Mutation and Type Coercion

--- js/complete-javascript-course-v1/2-JS-basics/script.js
+++ js/complete-javascript-course-v1/2-JS-basics/script.js
@@ -1,1 +1,50 @@
-console.log('hello world');
+/************************** 
+* Variables and data types
+*/
+/*
+var firstName = 'John';
+console.log(firstName);
+
+var lastName = 'Smith';
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
+
+// Variable naming rules
+var _3years = 3;
+var johnMark = 'John and Mark';
+var if = 23;
+
+*/
+
+/************************** 
+* Variables mutation and type coercion
+*/
+
+var firstName = 'John';
+var age = 28;
+
+// Type coercion
+console.log(firstName + ' ' + age);
+
+var job, isMarried;
+job = 'Teacher';
+isMarried = false;
+
+console.log(firstName + ' is a ' + age + ' year old ' + '. Is he married? ' + isMarried);
+
+// VAriable mutation
+age = 'twenty eight';
+job = 'driver';
+
+alert(firstName + ' is a ' + age + ' year old ' + '. Is he married? ' + isMarried);
+
+var lastName = prompt('What is his last Name?');
+console.log(firstName + ' ' + lastName);
