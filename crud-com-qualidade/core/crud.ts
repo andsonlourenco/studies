d8335003eb0fd9a94bddf8dbad2fd836da5f4cab
Andson Lourenco <andson-lourenco@hotmail.com>
2023-09-14 13:15:13 -0300

    creating crud-with-quality

---
+++ crud-com-qualidade/core/crud.ts
@@ -0,0 +1,121 @@
+import fs from "fs"; // ES6
+import { v4 as uuid } from "uuid";
+// const fs = require("fs"); - CommonJS
+const DB_FILE_PATH = "./core/db";
+
+type UUID = string;
+
+interface Todo {
+  id: UUID;
+  date: string;
+  content: string;
+  done: boolean;
+}
+
+function create(content: string): Todo {
+  const todo: Todo = {
+    id: uuid(),
+    date: new Date().toISOString(),
+    content: content,
+    done: false,
+  };
+
+  const todos: Array<Todo> = [...read(), todo];
+
+  fs.writeFileSync(
+    DB_FILE_PATH,
+    JSON.stringify(
+      {
+        todos,
+      },
+      null,
+      2
+    )
+  );
+  return todo;
+}
+
+export function read(): Array<Todo> {
+  const dbString = fs.readFileSync(DB_FILE_PATH, "utf-8");
+  const db = JSON.parse(dbString || "{}");
+  if (!db.todos) {
+    // Fail fast validations
+    return [];
+  }
+  return db.todos;
+}
+
+function update(id: UUID, partialTodo: Partial<Todo>): Todo {
+  let updateTodo;
+  const todos = read();
+
+  todos.forEach((currentTodo) => {
+    const isToUpdate = currentTodo.id === id;
+    if (isToUpdate) {
+      updateTodo = Object.assign(currentTodo, partialTodo);
+    }
+  });
+
+  fs.writeFileSync(
+    DB_FILE_PATH,
+    JSON.stringify(
+      {
+        todos,
+      },
+      null,
+      2
+    )
+  );
+
+  if (!updateTodo) {
+    throw new Error("Please, provider another ID!");
+  }
+
+  return updateTodo;
+}
+
+function updateContentById(id: UUID, content: string): Todo {
+  return update(id, {
+    content,
+  });
+}
+
+function deleteById(id: UUID) {
+  const todos = read();
+
+  const todosWithoutOne = todos.filter((todo) => {
+    if (id === todo.id) {
+      return false;
+    }
+
+    return true;
+  });
+
+  fs.writeFileSync(
+    DB_FILE_PATH,
+    JSON.stringify(
+      {
+        todos: todosWithoutOne,
+      },
+      null,
+      2
+    )
+  );
+}
+
+function CLEAN_DB() {
+  fs.writeFileSync(DB_FILE_PATH, "");
+}
+
+//SIMULATION
+// CLEAN_DB();
+
+// create("Primeira TODO");
+// const secondTodo = create("Primeira TODO");
+// deleteById(secondTodo.id);
+// const thirdTodo = create("Segundo TODO");
+// // update(thirdTodo.id, {
+// //     content: "Atualizada!",
+// //     done: true
+// // })
+// updateContentById(thirdTodo.id, "Atualizada");
