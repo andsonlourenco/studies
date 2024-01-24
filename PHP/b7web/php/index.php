61d90f4ad7ce6f5505029119c0e115a58ef7bee9
Andson Lourenco <andson-lourenco@hotmail.com>
2024-01-23 21:21:30 -0300

    Class - Exercício Prático (OO)

--- PHP/b7web/php/index.php
+++ PHP/b7web/php/index.php
@@ -39,13 +39,55 @@
 //echo "POST 1: ".$post1->likes." likes - ".$post1->getAuthor(). PHP_EOL;
 //echo "POST 1: ".$post2->likes." likes - ".$post2->getAuthor(). PHP_EOL;
 
+//class Matematica 
+//{
+//  public static function somar($x, $y)
+//  {
+//    return $x + $y;    
+//  }
+//}
+//
+//echo Matematica::somar(10, 20);
 
-class Matematica 
-{
-  public static function somar($x, $y)
-  {
-    return $x + $y;    
+class Calculadora{
+  public $number = 0;
+
+  public function add($value) {
+    $this->number += $value;
   }
+
+  public function sub($value){
+    $this->number -= $value;
+  }
+  
+  public function multiply($value){
+    $this->number *= $value;
+  }
+  public function divide($value){
+    $this->number /= $value;
+  }
+
+  public function total()
+  {
+    return $this->number;
+  }
+  
+  public function clear(){
+    return $this->number = 0;
+  }
+
 }
 
-echo Matematica::somar(10, 20);
+$calc = new Calculadora();
+$calc->add(12);
+$calc->add(2);
+$calc->sub(1);
+$calc->multiply(3);
+$calc->divide(2);
+$calc->add(0.5);
+
+echo "TOTAL: ".$calc->total().PHP_EOL;
+
+$calc->clear();
+
+echo "TOTAL: ".$calc->total();
