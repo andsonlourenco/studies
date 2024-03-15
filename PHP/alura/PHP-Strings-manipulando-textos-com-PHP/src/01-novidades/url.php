f208f69e59fc2611495678efd2dc01f851f8a71b
Andson Lourenco <andson-lourenco@hotmail.com>
2024-03-14 23:48:53 -0300

    class: início e fim

---
+++ PHP/alura/PHP-Strings-manipulando-textos-com-PHP/src/01-novidades/url.php
@@ -0,0 +1,19 @@
+<?php 
+
+$url = 'https://andsonlourenco.dev.br';
+
+if(str_starts_with($url, 'https')) {
+  echo 'É uma URL segura';
+}else {
+  echo 'Não é uma URL segura';
+}
+
+echo PHP_EOL;
+
+if(str_ends_with($url, '.br')){
+  echo 'É um domínio do Brasil';
+} else {
+  echo 'Não e um dominio do Brasil';
+}
+
+echo PHP_EOL;
