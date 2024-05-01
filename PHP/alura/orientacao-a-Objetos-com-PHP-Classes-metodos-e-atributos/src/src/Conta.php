3541476ead0d622b76a3a1c6108fb692231f4964
andson <andson-lourenco@hotmail.com>
2024-05-01 19:05:50 -0300

    Class - Criando o nosso primeiro método

--- PHP/alura/orientacao-a-Objetos-com-PHP-Classes-metodos-e-atributos/src/src/Conta.php
+++ PHP/alura/orientacao-a-Objetos-com-PHP-Classes-metodos-e-atributos/src/src/Conta.php
@@ -1,8 +1,17 @@
 <?php
 
-class Conta 
+class Conta
 {
   public $cpfTitular;
   public $nomeTitular;
   public $saldo;
+
+  public function sacar(float $valorASacar)
+  {
+    if ($valorASacar > $this->saldo) {
+      echo "Saldo indisponivel";
+    } else {
+      $this->saldo -= $valorASacar;
+    }
+  }
 }
