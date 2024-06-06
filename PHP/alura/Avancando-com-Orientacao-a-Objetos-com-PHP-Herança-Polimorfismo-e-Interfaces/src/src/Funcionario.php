c36c9907b5e99485d85d4d28473e339e81696cd6
andson <andson-lourenco@hotmail.com>
2024-06-05 21:42:39 -0300

    Class - Usando herança

--- PHP/alura/Avancando-com-Orientacao-a-Objetos-com-PHP-Herança-Polimorfismo-e-Interfaces/src/src/Funcionario.php
+++ PHP/alura/Avancando-com-Orientacao-a-Objetos-com-PHP-Herança-Polimorfismo-e-Interfaces/src/src/Funcionario.php
@@ -1,26 +1,15 @@
 <?php
 
-class Funcionario
+class Funcionario extends Pessoa
 {
-  private $nome;
-  private $cpf;
   private $cargo;
 
-  public function __construct(string $nome, string $cpf, string $cargo)
+  public function __construct(string $nome, CPF $cpf, string $cargo)
   {
-      $this->nome = $nome;
-      $this->cpf = $cpf;
-      $this->cargo = $cargo;
-  }
-
-  public function recuperaNome(): string
-  {
-      return $this->nome;
-  }
-
-  public function recuperaCpf(): string
-  {
-      return $this->cpf;
+    $this->validaNomeTitular($nome);
+    $this->nome = $nome;
+    $this->cpf = $cpf;
+    $this->cargo = $cargo;
   }
 
   public function recuperaCargo(): string
--- PHP/alura/Avancando-com-Orientacao-a-Objetos-com-PHP-Herança-Polimorfismo-e-Interfaces/src/src/Titular.php
+++ PHP/alura/Avancando-com-Orientacao-a-Objetos-com-PHP-Herança-Polimorfismo-e-Interfaces/src/src/Titular.php
@@ -1,9 +1,7 @@
 <?php
 
-class Titular
+class Titular extends Pessoa
 {
-  private $cpf;
-  private $nome;
   private $endereco;
 
   public function __construct(CPF $cpf, string $nome, Endereco $endereco)
@@ -11,24 +9,7 @@ class Titular
     $this->cpf = $cpf;
     $this->validaNomeTitular($nome);
     $this->nome = $nome;
-  }
-
-  public function recuperaCpf(): string
-  {
-    return $this->cpf->recuperaNumero();
-  }
-
-  public function recuperaNome(): string
-  {
-    return $this->nome;
-  }
-
-  private function validaNomeTitular(string $nomeTitular)
-  {
-    if (strlen($nomeTitular) < 5) {
-      echo "Nome precisa ter perlo menos 5 caracteres";
-      exit();
-    }
+    $this->endereco = $endereco;
   }
 
   public function recuperaEndereco(): Endereco
