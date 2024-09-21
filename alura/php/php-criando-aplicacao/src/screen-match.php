61469498e6f2cc26fc7885debbb6dd8350b2b00e
Andson Lourenco <andson-lourenco@hotmail.com>
2024-09-21 20:54:24 -0300

    Class - Outros loops

--- alura/php/php-criando-aplicacao/src/screen-match.php
+++ alura/php/php-criando-aplicacao/src/screen-match.php
@@ -3,26 +3,45 @@
 echo "Bem-vindo(a) ai screen match! <br>";
 
 $nomeFilme = "Top GUn - Maverick";
+$nomeFilme = "Thor: Ragnarok";
+$nomeFilme = "Se beber não case";
+
 $anoLancamento = $argv[1] ?? 2022;
 
-$somaDeNotas = 9;
-$somaDeNotas += 6;
-$somaDeNotas += 8;
-$somaDeNotas += 7.5;
-$somaDeNotas += 5;
+$quantidadeDeNotas = $argc - 1;
+$somaDeNotas = 0;
 
-$notaFilme = $somaDeNotas / 5;
+for ($contador = 1; $contador < $argc; $contador++ ) {
+  $somaDeNotas += $argv[$contador];
+}
+
+//$contador = 1;
+//while($argv[$contador] != 0){
+//  $somaDeNotas += $argv[$contador++];
+//}
+
+$notaFilme = $somaDeNotas / $quantidadeDeNotas;
 $planoPrime = true;
 $incluidoNoPlano = $planoPrime || $anoLancamento < 2020;
 
 echo "Nome do filme: ". $nomeFilme ."\n";
-echo "Nome do nota: $notaFilme \n";
+echo "Nota do filme: $notaFilme \n";
 echo "Ano de lançamento: $anoLancamento\n";
 
 if ($anoLancamento > 2022){
   echo "Esse filme é um lançamento \n";
 } elseif ($anoLancamento > 2020 && $anoLancamento <= 2022){
-  echo "Esse filme ainda é novo";
+  echo "Esse filme ainda é novo \n";
 } else {
-  echo "Esse filme não é um lançamento";
+  echo "Esse filme não é um lançamento \n";
 }
+
+$genero = match ($nomeFilme){ 
+  "Top Gun - Maverick" => "ação",
+  "Thor: Ragnarok" => "super-herói",
+  "Se beber não case" => "comédia",
+  default => "gênero desconhecido",
+};
+
+echo "O gênero do filme é: $genero \n";
+
