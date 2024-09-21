5980b9855dcbfff67bcd553b1dd5e963e78ca504
Andson Lourenco <andson-lourenco@hotmail.com>
2024-09-21 00:10:41 -0300

    Class - Operando sobre dados

--- alura/php/php-criando-aplicacao/src/screen-match.php
+++ alura/php/php-criando-aplicacao/src/screen-match.php
@@ -4,7 +4,18 @@ echo "Bem-vindo(a) ai screen match! <br>";
 
 $nomeFilme = "Top GUn - Maverick";
 $anoLancamento = 2022;
-$notaFilme = 8.8;
-$incluidoNoPlano = true;
 
-echo $nomeFilme;
+$somaDeNotas = 9;
+$somaDeNotas += 6;
+$somaDeNotas += 8;
+$somaDeNotas += 7.5;
+$somaDeNotas += 5;
+
+$notaFilme = $somaDeNotas / 5;
+$planoPrime = true;
+$incluidoNoPlano = $planoPrime || $anoLancamento < 2020;
+
+
+echo $nomeFilme . "<br />";
+
+echo $notaFilme;
