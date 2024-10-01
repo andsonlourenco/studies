032a1a14cdb3701332a6baa5c0a19e7ceb5f2502
Andson Lourenco <andson-lourenco@hotmail.com>
2024-10-01 16:46:43 -0300

    Class - Definindo ações para o filme

--- alura/php/PHP_evolua_seu_conhecimento_e_organize_seu_cdigo/src/screen-match/index.php
+++ alura/php/PHP_evolua_seu_conhecimento_e_organize_seu_cdigo/src/screen-match/index.php
@@ -1,59 +1,18 @@
 <?php
 require __DIR__ . "/src/Modelo/Filme.php";
-require __DIR__ . "/src/funcoes.php";
 
-echo "Bem-vindo(a) ai screen match! <br>";
+echo "Bem-vindo() ao ScreenMatch\n";
 
-$nomeFilme = "Top GUn - Maverick";
-$nomeFilme = "Thor: Ragnarok";
-$nomeFilme = "Se beber não case";
+$filme = new Filme();
+$filme->nome = 'Thor - Ragnarok';
+$filme->anoLancamento = 2021;
+$filme->genero = 'super-heroi';
 
-$anoLancamento = $argv[1] ?? 2022;
+$filme->avalia(10);
+$filme->avalia(6);
+$filme->avalia(7.8);
+$filme->avalia(8.2);
 
-$quantidadeDeNotas = $argc - 1;
-$notas = [];
+var_dump($filme);
 
-for ($contador = 1; $contador < $argc; $contador++ ) {
-  $notas[] += (float) $argv[$contador];
-}
-
-$notaFilme = array_sum($notas) / $quantidadeDeNotas;
-$planoPrime = true;
-
-$incluidoNoPlano = incluidoNoPlano($planoPrime, $anoLancamento);
-
-echo "Nome do filme: ". $nomeFilme ."\n";
-echo "Nota do filme: $notaFilme \n";
-echo "Ano de lançamento: $anoLancamento\n";
-
-exibeMensagemLancamento($anoLancamento);
-
-$genero = match ($nomeFilme){ 
-  "Top Gun - Maverick" => "ação",
-  "Thor: Ragnarok" => "super-herói",
-  "Se beber não case" => "comédia",
-  default => "gênero desconhecido",
-};
-
-echo "O gênero do filme é: $genero \n";
-
-$filme = criaFilme(
-  nome:"Thor: Ragnarok", 
-  anoLancamento: 2021, 
-  nota: 7.8, 
-  genero: "super-herói"
-);
-
-var_dump($filme->nome);
-$posicaoDoisPontos = strpos($filme->nome, ':');
-var_dump($posicaoDoisPontos);
-
-var_dump(substr($filme->nome, 0, $posicaoDoisPontos));
-echo "<br />";
-echo json_encode($filme);
-echo "<br />";
-var_dump(json_decode('{"nome":"Thor: Ragnarok","ano":2021,"nota":7.8,"genero":"super-her\u00f3i"}'), true);
-
-
-//$filmeComoStringJson = json_encode($filme);
-//file_put_contents(__DIR__ .'/filme.json', $filmeComoStringJson);
+echo $filme->media();
