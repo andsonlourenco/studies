47295034566c983247d9e28f2f5c1d5c26e20a1f
Andson Lourenco <andson-lourenco@hotmail.com>
2024-10-02 10:54:53 -0300

    Class - Extraindo o que é comum

--- alura/php/PHP_evolua_seu_conhecimento_e_organize_seu_cdigo/src/screen-match/src/Modelo/Serie.php
+++ alura/php/PHP_evolua_seu_conhecimento_e_organize_seu_cdigo/src/screen-match/src/Modelo/Serie.php
@@ -1,29 +1,13 @@
 <?php
 
-class Serie
+class Serie extends Titulo
 {
-  private array $notas;
-
   public function __construct(
-    public readonly string $nome,
-    public readonly int $anoLancamento,
-    public readonly Genero $genero,
-    public int $temporadas,
-    public int $episodiosPorTemporada,
-    public int $minutosPorEpisodio,
+    public readonly int $temporadas,
+    public readonly int $episodiosPorTemporada,
+    public readonly int $minutosPorEpisodio,
   ){
-    $this->notas = [];
-  }
-  public function avalia(float $nota): void
-  {
-    $this->notas[] = $nota;
+    
   }
 
-  public function media(): float
-  {
-    $somaNotas = array_sum($this->notas);
-    $quantidadeNotas = count($this->notas);
-
-    return $somaNotas / $quantidadeNotas;
-  }
 }
