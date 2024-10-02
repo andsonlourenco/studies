c8b35c1ca95c08a240ee3ef2ddd62d214c8e5ef2
Andson Lourenco <andson-lourenco@hotmail.com>
2024-10-02 19:54:32 -0300

    Class - Polimorfismo

--- alura/php/PHP_evolua_seu_conhecimento_e_organize_seu_cdigo/src/screen-match/index.php
+++ alura/php/PHP_evolua_seu_conhecimento_e_organize_seu_cdigo/src/screen-match/index.php
@@ -4,6 +4,7 @@ require __DIR__ . '/src/Modelo/Genero.php';
 require __DIR__ . '/src/Modelo/Titulo.php';
 require __DIR__ . "/src/Modelo/Filme.php";
 require __DIR__ . "/src/Modelo/Serie.php";
+require __DIR__ . "/src/Calculos/CalculadoraDeMaratona.php";
 
 echo "Bem-vindo() ao ScreenMatch\n";
 
@@ -30,3 +31,11 @@ $serie = new Serie('Lost', 2007, Genero::Drama, 10, 20, 30);
 echo $serie->anoLancamento . '\n';
 $serie->avalia(8);
 echo $serie->media() . '\n';
+
+
+$calculadora = new CalculadoraDeMaratona();
+$calculadora->inclui($filme);
+$calculadora->inclui($serie);
+$duracao = $calculadora->duracao();
+
+echo "Para essa maratona, você precisa de $duracao minutos";
