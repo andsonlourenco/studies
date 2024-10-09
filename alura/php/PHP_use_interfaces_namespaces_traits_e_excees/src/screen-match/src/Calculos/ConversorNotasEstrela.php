702f854ba09daac2829d902c41a55487f06b35dd
Andson Lourenco <andson-lourenco@hotmail.com>
2024-10-09 15:53:30 -0300

    Class - Tratamento de exceções

--- alura/php/PHP_use_interfaces_namespaces_traits_e_excees/src/screen-match/src/Calculos/ConversorNotasEstrela.php
+++ alura/php/PHP_use_interfaces_namespaces_traits_e_excees/src/screen-match/src/Calculos/ConversorNotasEstrela.php
@@ -2,14 +2,19 @@
 
 namespace ScreenMatch\Calculos;
 
+use DivisionByZeroError;
 use ScreenMatch\Modelo\Avaliavel;
 
 class ConversorNotasEstrela
 {
   public function converte(Avaliavel $avaliavel): float
   {
-    $nota = $avaliavel->media();
-    
-    return round($nota) / 2;
+    try{
+      $nota = $avaliavel->media();
+
+      return round($nota) / 2;
+    } catch(DivisionByZeroError $erro){
+      return 0;
+    }
   }
 }
