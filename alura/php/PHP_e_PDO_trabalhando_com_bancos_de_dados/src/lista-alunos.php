187b8ca8d182801030fb2f0623b59a5ce2a69987
Andson Lourenco <andson-lourenco@hotmail.com>
2024-10-17 12:10:31 -0300

    Class - Buscas específicas

--- alura/php/PHP_e_PDO_trabalhando_com_bancos_de_dados/src/lista-alunos.php
+++ alura/php/PHP_e_PDO_trabalhando_com_bancos_de_dados/src/lista-alunos.php
@@ -7,6 +7,16 @@ require_once 'vendor/autoload.php';
 $databasePath = __DIR__ . '/banco.sqlite';
 $pdo = new PDO('sqlite:'. $databasePath);
 
+//$statement = $pdo->query('SELECT * FROM students WHERE id = 1;');
+//while ($studentData = $statement->fetch(PDO::FETCH_ASSOC)) {
+//  $student = new Student(
+//    $studentData['id'],
+//    $studentData['name'],
+//    new \DateTimeImmutable($studentData['birth_date'])
+//  );
+//
+//  echo $student->age() . PHP_EOL;
+//}
 $statement = $pdo->query('SELECT * FROM students;');
 $studentDataList = $statement->fetchAll(PDO::FETCH_ASSOC);
 $studentList = [];
