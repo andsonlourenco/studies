c125cda61f569b54d0b442a67fce5747ab2ddcae
Andson Lourenco <andson-lourenco@hotmail.com>
2024-10-18 00:12:19 -0300

    Class - Tipos de parâmetros

--- alura/php/PHP_e_PDO_trabalhando_com_bancos_de_dados/src/inserir-aluno.php
+++ alura/php/PHP_e_PDO_trabalhando_com_bancos_de_dados/src/inserir-aluno.php
@@ -7,12 +7,12 @@ require_once 'vendor/autoload.php';
 $databasePath = __DIR__ . '/banco.sqlite';
 $pdo = new PDO('sqlite:'. $databasePath);
 
-$student = new Student(null, 'Andson', new \DateTimeImmutable('1992-11-19'));
+$student = new Student(null, 'New User', new \DateTimeImmutable('1993-11-19'));
 
-$sqlInsert = "INSERT INTO students (name, birth_date) VALUES (?, ?)";
+$sqlInsert = "INSERT INTO students (name, birth_date) VALUES (:name, :birth_date);";
 $statement = $pdo->prepare($sqlInsert);
-$statement->bindValue(1, $student->name());
-$statement->bindValue(2, $student->birthDate()->format('Y-m-d'));
+$statement->bindValue(':name', $student->name());
+$statement->bindValue(':birth_date', $student->birthDate()->format('Y-m-d'));
 
 if($statement->execute()){
   echo "Alunos Incluido!";
