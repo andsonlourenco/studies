f4ad5b6acd128112a22da2cb26ce5c7b31c338f3
Andson Lourenco <andson-lourenco@hotmail.com>
2024-10-24 15:23:53 -0300

    Class - Repositório de produtos

--- alura/php/PHP_e_MySQL_criando_sua_primeira_aplicao_web/src/index.php
+++ alura/php/PHP_e_MySQL_criando_sua_primeira_aplicao_web/src/index.php
@@ -1,35 +1,11 @@
 <?php
   require "src/conexao-bd.php";
   require "src/modelo/Produto.php";
+  require "src/Repositorio/ProdutoRepositorio.php";
 
-  $sql1 = "SELECT * FROM produtos WHERE tipo = 'Café' ORDER BY preco";
-  $statement = $pdo->query($sql1);
-  $produtosCafe = $statement->fetchAll(PDO::FETCH_ASSOC);
-  $dadosCafe = array_map(function ($cafe){
-    return new Produto(
-      $cafe['id'],
-      $cafe['tipo'],
-      $cafe['nome'],
-      $cafe['descricao'],
-      $cafe['imagem'],
-      $cafe['preco']
-  );
-}, $produtosCafe);
-
-$sql2 = "SELECT * FROM produtos WHERE tipo = 'Almoço' ORDER BY preco";
-$statement = $pdo->query($sql2);
-$produtosAlmoco = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-$dadosAlmoco = array_map(function ($almoco){
-  return new Produto(
-    $almoco['id'],
-    $almoco['tipo'],
-    $almoco['nome'],
-    $almoco['descricao'],
-    $almoco['imagem'],
-    $almoco['preco']
-);
-}, $produtosAlmoco);
+  $produtoRepositorio = new ProdutoRepositorio($pdo);
+  $dadosCafe = $produtoRepositorio->opcoesCafe();
+  $dadosAlmoco = $produtoRepositorio->opcoesAlmoco();
 
 ?>
 <!doctype html>
