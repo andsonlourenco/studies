1d075481d8542257f3efda8b16b0fcff57474c04
Andson Lourenco <andson-lourenco@hotmail.com>
2024-10-28 15:10:28 -0300

    Class - Realizando a busca

--- alura/php/PHP_na_Web_conhecendo_o_padro_mvc/src/novo-video.php
+++ alura/php/PHP_na_Web_conhecendo_o_padro_mvc/src/novo-video.php
@@ -8,4 +8,8 @@ $statement = $pdo->prepare($sql);
 $statement->bindValue(1, $_POST['url']);
 $statement->bindValue(2, $_POST['titulo']);
 
-$statement->execute();
+if($statement->execute() === false){
+  header('Location: /index.php?sucesso=0');
+} else {
+  header('Location: /index.php?sucesso=1');
+}
