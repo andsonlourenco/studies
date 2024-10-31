225bc8fd1f2d1f5f16975ab17b955decb20b4dec
Andson Lourenco <andson-lourenco@hotmail.com>
2024-10-31 00:38:20 -0300

    Class - Pasta public

--- alura/php/PHP_na_Web_conhecendo_o_padro_mvc/src/public/index.php
+++ alura/php/PHP_na_Web_conhecendo_o_padro_mvc/src/public/index.php
@@ -4,21 +4,21 @@ declare(strict_types=1);
 $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 if ($path === '/' || $path === '') {
-    require_once 'listagem-videos.php';
+    require_once __DIR__ . '/../listagem-videos.php';
 } elseif ($path === '/novo-video') {
     if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-        require_once 'formulario.php';
+        require_once __DIR__ . '/../formulario.php';
     } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        require_once 'novo-video.php';
+        require_once __DIR__ . '/../novo-video.php';
     }
 } elseif ($path === '/editar-video') {
     if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-        require_once 'formulario.php';
+        require_once __DIR__ . '/../formulario.php';
     } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        require_once 'editar-video.php';
+        require_once __DIR__ . '/../editar-video.php';
     }
 } elseif ($path === '/remover-video') {
-    require_once 'remover-video.php';
+    require_once __DIR__ . '/../remover-video.php';
 } else {
     http_response_code(404);
     echo "Página não encontrada";
