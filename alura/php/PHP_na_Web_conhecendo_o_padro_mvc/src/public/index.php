3a3d3acafb631825ba1b599561d22ee20a8bf057
Andson Lourenco <andson-lourenco@hotmail.com>
2024-11-01 02:11:34 -0300

    Class - Isolando o HTML

--- alura/php/PHP_na_Web_conhecendo_o_padro_mvc/src/public/index.php
+++ alura/php/PHP_na_Web_conhecendo_o_padro_mvc/src/public/index.php
@@ -1,36 +1,41 @@
 <?php
 declare(strict_types=1);
 
-use Alura\Mvc\Controller\VideoListController;
+use Alura\Mvc\Controller\{
+  Controller,
+  DeleteVideoController,
+  EditVideoController,
+  Error404Controller,
+  NewVideoController,
+  VideoFormController,
+  VideoListController
+};
 use Alura\Mvc\Repository\VideoRepository;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-
 $dbPath = __DIR__ . '/../banco.sqlite';
 $pdo = new PDO("sqlite:$dbPath");
 $videoRepository = new VideoRepository($pdo);
 
 if ($path === '/' || $path === '') {
   $controller = new VideoListController($videoRepository);
-  $controller->processaRequisicao();
-
 } elseif ($path === '/novo-video') {
-    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-        require_once __DIR__ . '/../formulario.php';
-    } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        require_once __DIR__ . '/../novo-video.php';
-    }
+  if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    $controller = new VideoFormController($videoRepository);
+  } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $controller = new NewVideoController($videoRepository);
+  }
 } elseif ($path === '/editar-video') {
-    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-        require_once __DIR__ . '/../formulario.php';
-    } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        require_once __DIR__ . '/../editar-video.php';
-    }
+  if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+      $controller = new VideoFormController($videoRepository);
+  } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
+      $controller = new EditVideoController($videoRepository);
+  }
 } elseif ($path === '/remover-video') {
-    require_once __DIR__ . '/../remover-video.php';
+  $controller = new DeleteVideoController($videoRepository);
 } else {
-    http_response_code(404);
-    echo "Página não encontrada";
+  $controller = new Error404Controller();
+  echo "Página não encontrada";
 }
