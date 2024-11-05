8ca9afc81e0985ba8eae695dd19493e8599bd339
Andson Lourenco <andson-lourenco@hotmail.com>
2024-11-04 22:05:09 -0300

    Class - Recebendo uploads

--- alura/php/PHP_na_Web_lidando_com_segurana_e_api/src/views/video-form.php
+++ alura/php/PHP_na_Web_lidando_com_segurana_e_api/src/views/video-form.php
@@ -1,6 +1,6 @@
 <?php require_once __DIR__ . '/inicio-html.php'; ?>
   <main class="container">
-    <form class="container__formulario" method="post">
+    <form class="container__formulario" method="post" enctype="multipart/form-data">
       <h2 class="formulario__titulo">Envie um vídeo!</h2>
         <div class="formulario__campo">
           <label class="campo__etiqueta" for="url">Link embed</label>
@@ -20,6 +20,16 @@
                   required
                   placeholder="Neste campo, dê o nome do vídeo"
                   id='titulo' />
+        </div>
+
+        <div class="formulario__campo">
+          <label class="campo__etiqueta" for="image">Imagem do vídeo</label>
+          <input name="image"
+                  type="file"
+                  accept="image/*"
+                  class="campo__escrita"
+                  required
+                  id='image' />
         </div>
 
         <input class="formulario__botao" type="submit" value="Enviar" />
