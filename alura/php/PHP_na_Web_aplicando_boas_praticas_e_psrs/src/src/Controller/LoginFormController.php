b1df90d39738648b95d06186633c28f13c14c56b
Andson Lourenco <andson-lourenco@hotmail.com>
2024-11-07 13:21:32 -0300

    Class - Chamada do HTML

--- alura/php/PHP_na_Web_aplicando_boas_praticas_e_psrs/src/src/Controller/LoginFormController.php
+++ alura/php/PHP_na_Web_aplicando_boas_praticas_e_psrs/src/src/Controller/LoginFormController.php
@@ -4,13 +4,13 @@ declare(strict_types=1);
 
 namespace Alura\Mvc\Controller;
 
-class LoginFormController implements Controller
+class LoginFormController extends ControllerWithHtml implements Controller
 {
   public function processaRequisicao(): void
   {
     if(array_key_exists('logado', $_SESSION) && $_SESSION['logado'] === true){
       header('Location: /');
     }
-    require_once __DIR__ . '/../../views/login-form.php';
+    $this->renderTemplate('login-form');
   }
 }
