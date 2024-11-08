d79f3851f54d942d474594957eb7a8476f3305c3
Andson Lourenco <andson-lourenco@hotmail.com>
2024-11-08 12:57:45 -0300

    CLass - Trait de View

--- alura/php/PHP_na_Web_aplicando_boas_praticas_e_psrs/src/src/Controller/VideoFormController.php
+++ alura/php/PHP_na_Web_aplicando_boas_praticas_e_psrs/src/src/Controller/VideoFormController.php
@@ -5,22 +5,25 @@ declare(strict_types=1);
 namespace Alura\Mvc\Controller;
 
 use Alura\Mvc\Entity\Video;
+use Alura\Mvc\Helper\HtmlRendererTrait;
 use Alura\Mvc\Repository\VideoRepository;
 
-class VideoFormController extends ControllerWithHtml implements Controller
+class VideoFormController implements Controller
 {
-    public function __construct(private VideoRepository $repository)
-    {
-    }
+  use HtmlRendererTrait;
+  
+  public function __construct(private VideoRepository $repository)
+  {
+  }
 
-    public function processaRequisicao(): void
-    {
-      $id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
-      /** @var ?Video $video */
-      $video = null;
-      if ($id !== false && $id !== null) {
-          $video = $this->repository->find($id);
-      }
-      echo $this->renderTemplate('video-form', ['video' => $video]);
+  public function processaRequisicao(): void
+  {
+    $id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+    /** @var ?Video $video */
+    $video = null;
+    if ($id !== false && $id !== null) {
+        $video = $this->repository->find($id);
     }
+    echo $this->renderTemplate('video-form', ['video' => $video]);
+  }
 }
