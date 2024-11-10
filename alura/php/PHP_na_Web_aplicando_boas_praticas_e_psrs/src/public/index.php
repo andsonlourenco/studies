6dcb540bf988eaea5ce51a92732d6fdd770c231a
Andson Lourenco <andson-lourenco@hotmail.com>
2024-11-10 02:26:06 -0300

    Class - Conhecendo a PSR-11

--- alura/php/PHP_na_Web_aplicando_boas_praticas_e_psrs/src/public/index.php
+++ alura/php/PHP_na_Web_aplicando_boas_praticas_e_psrs/src/public/index.php
@@ -13,6 +13,8 @@ $pdo = new PDO("sqlite:$dbPath");
 $videoRepository = new VideoRepository($pdo);
 
 $routes = require_once __DIR__ . '/../config/routes.php';
+/** @var \Psr\Container\ContainerInterface $diContainer*/
+$diContainer = require_once __DIR__ . '/../config/dependencies.php';
 
 $requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $pathInfo = $requestUri;
@@ -30,7 +32,7 @@ $key = "$httpMethod|$pathInfo";
 if(array_key_exists($key, $routes)){
   $controllerClass = $routes["$httpMethod|$pathInfo"];
   
-  $controller = new $controllerClass($videoRepository);
+  $controller = $diContainer->get($controllerClass);
 }else{
   $controller = new Error404Controller();
 }
