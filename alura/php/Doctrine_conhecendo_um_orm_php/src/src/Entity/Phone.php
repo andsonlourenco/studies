68f88b2676b2b9d329993d83007134c0490d3ecd
Andson Lourenco <andson-lourenco@hotmail.com>
2024-11-20 01:17:06 -0300

    Class - Mapeando as novidades

--- alura/php/Doctrine_conhecendo_um_orm_php/src/src/Entity/Phone.php
+++ alura/php/Doctrine_conhecendo_um_orm_php/src/src/Entity/Phone.php
@@ -6,19 +6,25 @@ use Doctrine\ORM\Mapping\Column;
 use Doctrine\ORM\Mapping\Entity;
 use Doctrine\ORM\Mapping\GeneratedValue;
 use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\ManyToOne;
 
 #[Entity]
 class Phone
 {
-  #[Id]
-  #[GeneratedValue]
-  #[Column]
-  public readonly int $id;
+  #[Id, GeneratedValue, Column]
+  public int $id;
+
+  #[ManyToOne(targetEntity: Phone::class, inversedBy: "phones")]
+  public readonly iterable $student;
 
   public function __construct(
     #[Column]
     public readonly string $number
-  )
+  ) {
+  }
+
+  public function setSudent(Student $student): void
   {
+    $this->student = $student;
   }
 }
