1c5f93823de8d91d92fe0abbc47bf4088fcb5ba5
Andson Lourenco <andson-lourenco@hotmail.com>
2024-11-20 23:37:49 -0300

    Class - Trabalhando com coleções

--- alura/php/Doctrine_conhecendo_um_orm_php/src/bin/list-students.php
+++ alura/php/Doctrine_conhecendo_um_orm_php/src/bin/list-students.php
@@ -4,6 +4,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use Alura\Doctrine\Entity\Student;
 use Alura\Doctrine\Helper\EntityManagerCreator;
+use \Alura\Doctrine\Entity\Phone;
 
 $entityManager = EntityManagerCreator::createEntityManager();
 $studentRepository = $entityManager->getRepository(Student::class);
@@ -14,9 +15,7 @@ $studentList = $studentRepository->findAll();
 foreach ($studentList as $student) {
   echo "ID: $student->id\nNome: $student->name\n\n";
   echo "Telefones:\n";
-  foreach ($student->phones() as $phone) {
-    echo "  - $phone->number\n";
-  }
+  echo implode(', ', $student->phones()->map(fn (Phone $phone) => $phone->number)->toArray());
 
   echo "\n";
 }
