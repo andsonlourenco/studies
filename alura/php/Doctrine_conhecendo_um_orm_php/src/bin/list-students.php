304e6d6d8495bdde0dc8cf336f5fcff4d6e554fa
Andson Lourenco <andson-lourenco@hotmail.com>
2024-11-23 13:58:24 -0300

    Class - Matriculando alunos

--- alura/php/Doctrine_conhecendo_um_orm_php/src/bin/list-students.php
+++ alura/php/Doctrine_conhecendo_um_orm_php/src/bin/list-students.php
@@ -2,6 +2,7 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+use Alura\Doctrine\Entity\Course;
 use Alura\Doctrine\Entity\Student;
 use Alura\Doctrine\Helper\EntityManagerCreator;
 use \Alura\Doctrine\Entity\Phone;
@@ -13,11 +14,19 @@ $studentRepository = $entityManager->getRepository(Student::class);
 $studentList = $studentRepository->findAll();
 
 foreach ($studentList as $student) {
-  echo "ID: $student->id\nNome: $student->name\n\n";
-  echo "Telefones:\n";
-  echo implode(', ', $student->phones()->map(fn (Phone $phone) => $phone->number)->toArray());
-
-  echo "\n";
+  echo "ID: $student->id\nNome: $student->name";
+  
+  if($student->phones()->count() > 0){
+    echo "\n";
+    echo "Telefones:\n";
+    echo implode(', ', $student->phones()->map(fn (Phone $phone) => $phone->number)->toArray());
+  }
+  if($student->courses()->count() > 0){
+    echo "\n";
+    echo "Cursos:\n";
+    echo implode(', ', $student->courses()->map(fn (Course $course) => $course->name)->toArray());
+  }
+  echo "\n\n";
 }
 
 // /** @var Student $student */
