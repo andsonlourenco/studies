69cfcc3830a28a3bc74e5ca61e2ff003af214f6d
Andson Lourenco <andson-lourenco@hotmail.com>
2024-11-25 01:06:35 -0300

    Class - Instalando o Bootstrap

--- alura/php/laravel-criando-uma-aplicacao-com-mvc/src/resources/views/components/layout.blade.php
+++ alura/php/laravel-criando-uma-aplicacao-com-mvc/src/resources/views/components/layout.blade.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>{{$title}} - Conrole de Séries </title>
+  @vite(['resources/scss/app.scss', 'resources/js/app.js'])
 </head>
 <body>
   <h1>{{$title}}</h1>
