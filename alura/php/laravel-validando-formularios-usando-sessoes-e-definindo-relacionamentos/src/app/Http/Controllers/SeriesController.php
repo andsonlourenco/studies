c4db1990c80283158d2e081504cb0adffae3ecf5
Andson Lourenco <andson-lourenco@hotmail.com>
2024-12-06 13:10:08 -0300

    Class - Bulk insert

--- alura/php/laravel-validando-formularios-usando-sessoes-e-definindo-relacionamentos/src/app/Http/Controllers/SeriesController.php
+++ alura/php/laravel-validando-formularios-usando-sessoes-e-definindo-relacionamentos/src/app/Http/Controllers/SeriesController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\SeriesFormRequest;
+use App\Models\Episode;
 use App\Models\Series;
+use App\Models\Season;
 use Illuminate\Http\Request;
 
 class SeriesController extends Controller
@@ -25,6 +27,26 @@ class SeriesController extends Controller
     public function store(SeriesFormRequest $request)
     {
         $serie = Series::create($request->all());
+        $seasons = [];
+
+        for($i =1; $i <= $request->seasonsQty; $i++){
+            $seasons[] = [
+                'series_id' => $serie->id,
+                'number' => $i,
+            ];
+        }
+        Season::insert($seasons);
+        
+        $episodes = [];
+        foreach ($serie->seasons as $season){
+            for ($j = 1; $j <= $request->episodesPerSeason; $j++){
+                $episodes[] = [
+                    'season_id' => $season->id,
+                    'number' => $j,
+                ];
+            }
+        }
+        Episode::insert($episodes);
 
         return to_route('series.index')->with('mensagem.sucesso', "Série {$serie->nome} adicionada com sucesso");
     }
