de7ff2c45e6fffcb81f27029fd4bda354abecde7
Andson Lourenco <andson-lourenco@hotmail.com>
2024-12-08 00:18:08 -0300

    start course Laravel: transações, service container e autenticação and class DB::transaction

---
+++ alura/php/laravel-transacoes-service-container-autenticacao/src/resources/views/components/layout.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>{{$title}} - Conrole de Séries </title>
+  @vite(['resources/scss/app.scss', 'resources/js/app.js'])
+</head>
+<body class="container">
+  <h1>{{$title}}</h1>
+
+  @if ($errors->any())
+    <div class="alert alert-danger">
+      <ul>
+        @foreach ($errors->all() as $error)
+          <li>{{ $error }}</li>
+        @endforeach
+      </ul>
+    </div>
+  @endif
+
+  {{$slot}}
+</body>
+</html>
