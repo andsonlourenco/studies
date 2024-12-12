eb5447b2735af6ab2ad399ccdbb568475ead6fee
Andson Lourenco <andson-lourenco@hotmail.com>
2024-12-12 19:56:34 -0300

    Start course  Laravel: e-mails, eventos assíncronos, uploads e testes and class - Criando um e-mail

---
+++ alura/php/laravel-e-mails-eventos-assincronos-uploads-e-testes/src/resources/views/components/layout.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>{{$title}} - Conrole de Séries </title>
+  @vite(['resources/scss/app.scss', 'resources/js/app.js'])
+</head>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="{{ route('series.index') }}">Home</a>
+
+    @auth
+      <form action="{{ route('logout') }}" method="post">
+        @csrf
+        <button class="btn btn-link">
+          Sair
+        </button>
+      </form>
+    @endauth
+
+    @guest
+      <a href="{{ route('login') }}">Entrar</a>
+    @endguest
+  </div>
+</nav>
+
+<body class="container">
+  <h1>{{$title}}</h1>
+
+  @isset($mensagemSucesso)
+    <div class="alert alert-success">
+      {{ $mensagemSucesso }}
+    </div>
+  @endisset
+
+  @if ($errors->any())
+    <div class="alert alert-danger">
+      <ul>
+        @foreach ($errors->all() as $error)
+          <li>{{ $error }}</li>
+        @endforeach
+      </ul>
+    </div>
+  @endif
+
+  {{$slot}}
+</body>
+</html>
