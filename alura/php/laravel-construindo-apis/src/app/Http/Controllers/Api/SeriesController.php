fd917ac632cccf72c1b6f75a29f308da2c6c2f13
Andson Lourenco <andson-lourenco@hotmail.com>
2024-12-31 02:44:58 -0300

    CLass - GET de série

--- alura/php/laravel-construindo-apis/src/app/Http/Controllers/Api/SeriesController.php
+++ alura/php/laravel-construindo-apis/src/app/Http/Controllers/Api/SeriesController.php
@@ -5,9 +5,14 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\SeriesFormRequest;
 use App\Models\Series;
+use App\Repositories\SeriesRepository;
 
-class SeriesCOntroller extends Controller
+class SeriesController extends Controller
 {
+    public function __construct(private SeriesRepository $seriesRepository)
+    {
+
+    }
     public function index()
     {
         return Series::all();
@@ -15,6 +20,18 @@ class SeriesCOntroller extends Controller
 
     public function store(SeriesFormRequest $request)
     {
-        return response()->json(Series::create($request->all()), 201);
+        return response()->json($this->seriesRepository->add($request), 201);
+    }
+
+    //exemplo de eager loading de vários relacionamentos
+    // public function show(int $series)
+    // {
+    //     $series = Series::whereId($series)->with('seasons.episodes')->first();
+    //     return $series;
+    // }
+
+    public function show(Series $series)
+    {
+        return $series;
     }
 }
