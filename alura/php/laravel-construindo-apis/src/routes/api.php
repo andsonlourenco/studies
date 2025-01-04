3218809d6f922e668db87e142928efb99bb38da6
Andson Lourenco <andson-lourenco@hotmail.com>
2025-01-03 23:42:24 -0300

    Class - Subrecursos

--- alura/php/laravel-construindo-apis/src/routes/api.php
+++ alura/php/laravel-construindo-apis/src/routes/api.php
@@ -1,6 +1,14 @@
 <?php
 
+use App\Models\Series;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('series', \App\Http\Controllers\Api\SeriesController::class);
+Route::get('series/{series}/seasons', function(Series $series){
+    return $series->seasons;
+});
+
+Route::get('series/{series}/episodes', function(Series $series){
+    return $series->episodes;
+});
