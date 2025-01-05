7aba15da1012b5cb011bf3a6ad706036cab0472c
Andson Lourenco <andson-lourenco@hotmail.com>
2025-01-04 22:10:51 -0300

    Class - Laravel Sanctum

--- alura/php/laravel-construindo-apis/src/routes/api.php
+++ alura/php/laravel-construindo-apis/src/routes/api.php
@@ -2,20 +2,35 @@
 
 use App\Models\Series;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('series', \App\Http\Controllers\Api\SeriesController::class);
-Route::get('series/{series}/seasons', function(Series $series){
-    return $series->seasons;
+Route::middleware('auth:sanctum')->group(function(){
+    Route::apiResource('series', \App\Http\Controllers\Api\SeriesController::class);
+    Route::get('series/{series}/seasons', function(Series $series){
+        return $series->seasons;
+    });
+
+    Route::get('series/{series}/episodes', function(Series $series){
+        return $series->episodes;
+    });
+
+    Route::patch('/episodes/{episode}', function (\App\Models\Episode $episode, Request $request) {
+        $episode->watched = $request->watched;
+        $episode->save();
+
+        return $episode;
+    });
 });
 
-Route::get('series/{series}/episodes', function(Series $series){
-    return $series->episodes;
-});
+Route::post('/login', function(Request $request){
+    $credentials = $request->only(['email', 'password']);
+    if(Auth::attempt($credentials) === false){
+        return response()->json('Unauthorized', 401);
+    }
 
-Route::patch('/episodes/{episode}', function (\App\Models\Episode $episode, Request $request) {
-    $episode->watched = $request->watched;
-    $episode->save();
+    $user = Auth::user();
+    $token = $user->createToken('token');
 
-    return $episode;
+    return response()->json($token->plainTextToken);
 });
